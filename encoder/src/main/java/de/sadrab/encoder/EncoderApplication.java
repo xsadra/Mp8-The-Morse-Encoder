0cfd412520d17343850ad609a84bab27927f1236
Sadra <ti9mail@gmail.com>
2018-04-04 09:51:53 +0200

    Add Spring structure

---
+++ encoder/src/main/java/de/sadrab/encoder/EncoderApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncoderApplication.class, args);
+	}
+}
